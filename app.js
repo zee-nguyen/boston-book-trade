f3f95930999e28eb57cb45bec9f5c5eb1f583ee5
Zi Nguyen <zi@zinguyen.com>
2018-01-04 20:52:21 +0000

    separate book and comment models

--- app.js
+++ app.js
@@ -1,71 +1,71 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
+var express     = require("express"),
+    mongoose    = require("mongoose"),
+    bodyParser  = require("body-parser"),
+    app         = express(),
+    Book        = require("./models/book.js"),
+    seedDB      = require("./seeds");
 
+
+mongoose.connect("mongodb://localhost/book_trade");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
-var books = [
-        {
-            name: "Dataclysm",
-            image: "https://images-na.ssl-images-amazon.com/images/I/41p%2B6zsrd%2BL.jpg"
-        },
-        {
-            name: "Made to Stick",
-            image: "https://images-na.ssl-images-amazon.com/images/I/517I-tX7ThL._SX332_BO1,204,203,200_.jpg"
-        },
-        {
-            name: "Sapiens",
-            image: "https://images-na.ssl-images-amazon.com/images/I/41MJX6yzfeL._SX324_BO1,204,203,200_.jpg"
-        },
-        {
-            name: "Dataclysm",
-            image: "https://images-na.ssl-images-amazon.com/images/I/41p%2B6zsrd%2BL.jpg"
-        },
-        {
-            name: "Made to Stick",
-            image: "https://images-na.ssl-images-amazon.com/images/I/517I-tX7ThL._SX332_BO1,204,203,200_.jpg"
-        },
-        {
-            name: "Sapiens",
-            image: "https://images-na.ssl-images-amazon.com/images/I/41MJX6yzfeL._SX324_BO1,204,203,200_.jpg"
-        },
-        {
-            name: "Dataclysm",
-            image: "https://images-na.ssl-images-amazon.com/images/I/41p%2B6zsrd%2BL.jpg"
-        },
-        {
-            name: "Made to Stick",
-            image: "https://images-na.ssl-images-amazon.com/images/I/517I-tX7ThL._SX332_BO1,204,203,200_.jpg"
-        },
-        {
-            name: "Sapiens",
-            image: "https://images-na.ssl-images-amazon.com/images/I/41MJX6yzfeL._SX324_BO1,204,203,200_.jpg"
-        }
-       ];
-       
-       
+seedDB();
+
+//Display landing page
 app.get("/", function(req, res){
    res.render("landing"); 
 });
 
+//INDEX -- Display all current book trade
 app.get("/books", function(req, res){
-       res.render("books", {books: books});
+    //Get all books from DB
+    Book.find({}, function(err, allBooks){
+        if (err) {
+            console.log(err);
+        } else {
+            //render books page
+            res.render("index", {books: allBooks});
+        }
+    });
 });
 
+//NEW -- Display form to create a new book trade
 app.get("/books/new", function(req, res) {
    res.render("new");
 });
 
+//CREATE -- create new book and add to DB
 app.post("/books", function(req, res) {
     //get data from form and add to books array
-    var name = req.body.name;
+    var title = req.body.title;
     var image = req.body.image;
-    var newBook = {name: name, image: image};
-    books.push(newBook);
-    //redirect user to books page
-    res.redirect("/books");
+    var desc = req.body.description;
+    //create new book
+    var newBook = {title: title, image: image, description: desc};
+    Book.create(newBook, function(err, newlyCreatedBook){
+        if (err) {
+            console.log (err);
+        } else {
+            //redirect user to books page
+            res.redirect("/books");
+        }
+    });
+});
+
+//SHOW -- Display more info about one book
+app.get("/books/:id", function(req, res){
+   //find the book with provided ID
+   Book.findById(req.params.id).populate("comments").exec(function(err, foundBook) {
+       if (err) {
+           console.log(err)
+       } else {
+           console.log(foundBook);
+           //render show template with that ID
+           res.render("show", {book: foundBook});     
+       }
+   });
 });
 
 
