a443911c9762793f06e5e91bebea1a5c1e32de9a
Zi Nguyen <zinguyenn@gmail.com>
2018-01-08 19:50:32 +0000

    modified book model to include author

--- models/book.js
+++ models/book.js
@@ -4,6 +4,13 @@ var bookSchema = new mongoose.Schema({
     title: String,
     image: String,
     description: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    },
     comments: [
         {
             type: mongoose.Schema.Types.ObjectId,
