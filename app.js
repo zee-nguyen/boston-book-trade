ac92ba4c8a2c087d4f551d8c87de2ccf30abb1ed
Zi Nguyen <zinguyenn@gmail.com>
2018-01-08 14:28:15 +0000

    refractoring the routes and move them to separate files

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ var express                 = require("express"),
     User                    = require("./models/user"),
     seedDB                  = require("./seeds");
 
+//requiring routes
+var indexRoutes         = require("./routes/index"),
+    bookRoutes          = require("./routes/books"),
+    commentRoutes       = require("./routes/comments");
+    
 
 mongoose.connect("mongodb://localhost/book_trade", {useMongoClient: true});
 mongoose.Promise = global.Promise;
@@ -40,162 +45,10 @@ app.use(function(req, res, next) {
 
 seedDB();
 
-//Display landing page
-app.get("/", function(req, res){
-   res.render("landing"); 
-});
+app.use("/", indexRoutes);
+app.use("/books", bookRoutes);
+app.use("/books/:id/comments", commentRoutes);
 
-//INDEX -- Display all current book trade
-app.get("/books", function(req, res){
-    //Get all books from DB
-    Book.find({}, function(err, allBooks){
-        if (err) {
-            console.log(err);
-        } else {
-            //render books page
-            res.render("books/index", {books: allBooks});
-        }
-    });
-});
-
-//NEW -- Display form to create a new book trade
-app.get("/books/new", function(req, res) {
-   res.render("books/new");
-});
-
-//CREATE -- create new book and add to DB
-app.post("/books", function(req, res) {
-    //get data from form and add to books array
-    var title = req.body.title;
-    var image = req.body.image;
-    var desc = req.body.description;
-    //create new book
-    var newBook = {title: title, image: image, description: desc};
-    Book.create(newBook, function(err, newlyCreatedBook){
-        if (err) {
-            console.log (err);
-        } else {
-            //redirect user to books page
-            res.redirect("books/index");
-        }
-    });
-});
-
-//SHOW -- Display more info about one book
-app.get("/books/:id", function(req, res){
-   //find the book with provided ID
-   Book.findById(req.params.id).populate("comments").exec(function(err, foundBook) {
-       if (err) {
-           console.log(err)
-       } else {
-           console.log(foundBook);
-           //render show template with that ID
-           res.render("books/show", {book: foundBook});     
-       }
-   });
-});
-
-// ==========================
-// COMMENT ROUTE
-// ==========================
-//NEW comments -- display form to create new comment
-app.get("/books/:id/comments/new", isLoggedIn, function(req, res) {
-    //find book by id
-    Book.findById(req.params.id, function(err, foundBook) {
-        if (err) {
-            console.log(err)
-        } else {
-            //render page 
-            res.render("comments/new", {book: foundBook});
-        }
-    })
-});
-
-//CREATE comments -- create new comment and add to db
-app.post("/books/:id/comments", isLoggedIn, function(req, res) {
-    //look up book by id
-    Book.findById(req.params.id, function(err, book) {
-        if(err) {
-            console.log(err);
-            res.redirect("/books");
-        } else {
-            //create new comment
-            Comment.create(req.body.comment, function(err, comment) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    //add comment to book
-                    book.comments.push(comment);
-                    book.save();
-                    //redirect to show page
-                    res.redirect("/books/" + book._id);
-                }
-            });
-        }
-    });
-})
-
-
-// ==========================
-// AUTH ROUTE
-// ==========================
-//Show register form
-app.get("/register", function(req, res) {
-    res.render("register");
-});
-
-//handle user sign up
-app.post("/register", function(req, res) {
-    var newUser = new User({username: req.body.username});
-  User.register(newUser, req.body.password, function(err, user) {
-      if (err) {
-          console.log(err);
-          return res.redirect("/register");
-      } 
-      passport.authenticate("local")(req, res, function(){
-          res.redirect("/books");
-      });
-  });
-});
-
-//show login form
-app.get("/login", function(req, res){
-    res.render("login");
-})
-
-//handle user login
-//app.post("/login", callback)
-app.post("/login", function(req, res, next) {
-    passport.authenticate("local", function(err, user, info) {
-        if (err) {
-            return next(err);
-        }
-        if (!user) {
-            return res.redirect("/login");
-        }
-        req.login(user, function(err) {
-            if (err) { return next(err) }
-            var redirectTo = req.session.redirectTo ? req.session.redirectTo : "/books";
-            delete req.session.redirectTo;
-            res.redirect(redirectTo);
-        });
-    })(req, res, next);
-});
-
-//user logout
-app.get("/logout", function(req, res) {
-    req.logout();
-    res.redirect("/");
-});
-
-//middleware
-function isLoggedIn (req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    req.session.redirectTo = req.originalUrl;
-    res.redirect("/login");
-}
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started!");
