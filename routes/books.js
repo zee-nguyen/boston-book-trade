d6ae5067d0b6d086d0ef64e9a5385250a0a5ae46
Zi Nguyen <zinguyenn@gmail.com>
2018-01-09 22:18:34 +0000

    required middleware

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,8 @@
-var express = require("express"),
-    router  = express.Router(),
-    Book    = require("../models/book.js");
+var express     = require("express"),
+    router      = express.Router(),
+    Book        = require("../models/book.js"),
+    //middleware = require("../middleware/index.js") -- code will automatically look into index
+    middleware  = require("../middleware");
 
 //INDEX -- Display all current book trade
 router.get("/", function(req, res){
@@ -16,12 +18,12 @@ router.get("/", function(req, res){
 });
 
 //NEW -- Display form to create a new book trade
-router.get("/new", isLoggedIn, function (req, res) {
+router.get("/new", middleware.isLoggedIn, function (req, res) {
    res.render("books/new");
 });
 
 //CREATE -- create new book and add to DB
-router.post("/", isLoggedIn, function (req, res) {
+router.post("/", middleware.isLoggedIn, function (req, res) {
     //get data from form and add to books array
     var title = req.body.title;
     var image = req.body.image;
@@ -56,14 +58,14 @@ router.get("/:id", function(req, res){
 });
 
 //EDIT book -- show edit form
-router.get("/:id/edit", checkBookOwnership, function (req, res) {
+router.get("/:id/edit", middleware.checkBookOwnership, function (req, res) {
     Book.findById(req.params.id, function(err, foundBook){
         res.render("books/edit", {book: foundBook});
     });
 });    
 
 //UPDATE book -- update book
-router.put("/:id", checkBookOwnership, function(req, res) {
+router.put("/:id", middleware.checkBookOwnership, function(req, res) {
     //find and update the correct book
     Book.findByIdAndUpdate(req.params.id, req.body.book, function(err, updatedBook) {
         if (err) {
@@ -76,7 +78,7 @@ router.put("/:id", checkBookOwnership, function(req, res) {
 });
 
 //DELETE book -- remove book from database
-router.delete("/:id", checkBookOwnership, function (req, res) {
+router.delete("/:id", middleware.checkBookOwnership, function (req, res) {
     Book.findByIdAndRemove(req.params.id, function(err) {
         if (err) {
             res.redirect("/books");
@@ -85,39 +87,5 @@ router.delete("/:id", checkBookOwnership, function (req, res) {
         }
     })
 });
-
-
-//middleware
-function isLoggedIn (req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    req.session.redirectTo = req.originalUrl;
-    res.redirect("/login");
-}
-
-function checkBookOwnership(req, res, next) {
-    //is the user logged in?
-    if (req.isAuthenticated()) {
-        Book.findById(req.params.id, function(err, foundBook){
-            if (err) {
-                //if err, redirect back to the page they came from
-                res.redirect("back");
-            } else {
-                // if the user is logged in, does user own the book?
-                //req.user._id is a string 
-                //foundBook.author.id is a mongoose object --> has to use .equals() to compare
-                if (foundBook.author.id.equals(req.user._id)) {
-                    //if so, proceed to the next step
-                    next();
-                } else {
-                    res.redirect("back");
-                }
-            }
-        });
-    } else {
-        res.redirect("back");
-    }
-};
 
 module.exports = router;
