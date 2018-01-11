747339181e83ff7b64b090c3706bc3629d5fcc2d
Zi Nguyen <zinguyenn@gmail.com>
2018-01-11 19:32:49 +0000

    config flash

--- routes/comments.js
+++ routes/comments.js
@@ -76,6 +76,7 @@ router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, re
        if (err) {
            res.redirect("back");
        } else {
+           req.flash("success", "Comment deleted.");
            res.redirect("/books/" + req.params.id);
        }
    })
